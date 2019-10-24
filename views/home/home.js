487f9072c978cd34d67f4ef2ac53997527a4069b
Jonas Delrue <jonas.delrue@jimber.org>
2019-10-24 16:42:20 +0200

    Adding fb and githun

--- views/home/home.js
+++ views/home/home.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   name: 'home',
   components: {},
@@ -23,10 +22,9 @@ module.exports = {
     var ui = new firebaseui.auth.AuthUI(firebase.auth())
     ui.start('#firebaseui-auth-container', {
       signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID
-        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        // firebase.auth.GithubAuthProvider.PROVIDER_ID
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+         firebase.auth.GithubAuthProvider.PROVIDER_ID
       ],
       signInSuccessUrl: `${window.location.href}profile`
     })
